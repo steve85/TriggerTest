1342d61555e6350ccbe74ef62de3f1cd14886ddf
Steve Ryan <steve@xpfbs72.net>
2010-10-12 12:41:24 +1100

    added override for ToString to person class

--- ConsoleApplication1/ConsoleApplication1/Person.cs
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -9,5 +9,12 @@ namespace ConsoleApplication1
     {
 		public String Name {get; set;}
 		public Int32 Age {get; set;}
+		
+		public override String ToString()
+		{
+			return String.Format("Hello, my name is {0} and I am {1} years old.",
+			this.Name,
+			this.Age);
+		}
     }
 }
