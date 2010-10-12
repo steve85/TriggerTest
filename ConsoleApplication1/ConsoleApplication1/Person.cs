c0fcd8025c32d6892596288d757e05371893e282
Steve Ryan <steve@xpfbs72.net>
2010-10-12 12:39:05 +1100

    added person class

---
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Person
+    {
+		public String Name {get; set;}
+		public Int32 Age {get; set;}
+    }
+}
