2a915f8b060cf24bd1c557e7880b9bc70c10a313
Steve Ryan <steve@xpfbs72.net>
2010-10-14 16:53:14 +1100

    Person.cs added IncrementAge method

--- ConsoleApplication1/ConsoleApplication1/Person.cs
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -15,10 +15,15 @@ namespace ConsoleApplication1
 		public override String ToString()
 		{
 			return String.Format("Hello, my name is {0} {1} and I am {2} years old. I am a {3}.",
-			this.FirstName,
-			this.LastName,
-			this.Age,
-			Sex == Gender.Male ? "Male" : "Female");
+				this.FirstName,
+				this.LastName,
+				this.Age,
+				Sex == Gender.Male ? "Male" : "Female");
+		}
+		
+		public void IncrementAge()
+		{
+			this.Age++;
 		}
     }
 }
